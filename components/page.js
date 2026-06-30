555614ad5da34d7f3e55eea2b8ee5b7e3efed888
nikoladubica <45574682+nikoladubica@users.noreply.github.com>
2026-06-30 11:07:10 +0200

    #27 Upgrade Redux Toolkit to v2 and React Redux to v9

--- components/page.js
+++ components/page.js
@@ -1,11 +1,4 @@
-import { useRouter } from "next/router"
-import { useState, useEffect } from "react"
-import { motion, AnimatePresence } from "framer-motion"
-import { useSelector } from "react-redux"
-
 const Page = ({ children }) => {
-    const router = useRouter()
-
     return (
         <main className='relative h-[calc(100vh-88px)]'>
             {children}
